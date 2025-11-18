0d5932e5dd73ba124a4478996422404d456f17b9
saudkhanbpk <saudkhanbpk@gmail.com>
2025-11-19 00:09:00 +0500

    new deployment

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async (mongoUri) => {
   try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoUri);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
